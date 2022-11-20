f1a5165d54e2ebc1b5831212326c5b76c2249d72
Henrique Chaves <henriquechavesmm@gmail.com>
2022-11-20 11:49:28 +0100

    Create Home, Player and Pokemon controllers

--- pokemon/routes/web.php
+++ pokemon/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\{HomeController, PlayerController, PokemonController};
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'show']);
+
+Route::get('/players', [PlayerController::class, 'show']);
+
+Route::get('/pokemons', [PokemonController::class, 'show']);
