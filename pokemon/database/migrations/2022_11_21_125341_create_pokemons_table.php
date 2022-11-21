30ba511ed46dd103500e373e6665b8b8edf309af
Henrique Chaves <henriquechavesmm@gmail.com>
2022-11-21 14:06:10 +0100

    Add migrations for energies, pokemons and players tables

---
+++ pokemon/database/migrations/2022_11_21_125341_create_pokemons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pokemons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('energy_id')->constrained('energies');
+            $table->integer('level');
+            $table->integer('max_health_points');
+            $table->integer('normal_damage');
+            $table->integer('special_damage');
+            $table->integer('special_defense');
+            $table->string('image_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pokemons');
+    }
+};
