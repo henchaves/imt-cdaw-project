d3687bbc1d953aa2c849828155b4f5109c68fc56
Henrique Chaves <henriquechavesmm@gmail.com>
2022-11-22 17:20:41 +0100

    Add more columns to Pokemons table

--- pokemon/app/Models/Pokemon.php
+++ pokemon/app/Models/Pokemon.php
@@ -10,4 +10,9 @@ class Pokemon extends Model
     use HasFactory;
     protected $table = 'pokemon';
     public $timestamps = false;
+
+    public function energy()
+    {
+        return $this->belongsTo(Energy::class);
+    }
 }
