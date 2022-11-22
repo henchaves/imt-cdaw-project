80a9ca297f3aad04c656bf7d7fb47e11d34bc561
Henrique Chaves <henriquechavesmm@gmail.com>
2022-11-22 17:30:49 +0100

    Create a seeder and factory for player table

--- pokemon/database/seeders/DatabaseSeeder.php
+++ pokemon/database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         EnergySeeder::run();
         PokemonSeeder::run();
+        PlayerSeeder::run();
     }
 }
---
+++ pokemon/database/seeders/PlayerSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PlayerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public static function run()
+    {
+        \App\Models\Player::factory(10)->create();
+    }
+}
