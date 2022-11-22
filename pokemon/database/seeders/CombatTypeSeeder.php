b42a904510d505aba387e3bfa290f6209b26b7dc
Henrique Chaves <henriquechavesmm@gmail.com>
2022-11-22 17:41:37 +0100

    Seed combat_type table

---
+++ pokemon/database/seeders/CombatTypeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CombatTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public static function run()
+    {
+        DB::table('combat_type')->insert([
+            'name' => 'manual',
+        ]);
+
+        DB::table('combat_type')->insert([
+            'name' => 'random',
+        ]);
+    }
+}
--- pokemon/database/seeders/DatabaseSeeder.php
+++ pokemon/database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         EnergySeeder::run();
         PokemonSeeder::run();
         PlayerSeeder::run();
+        CombatTypeSeeder::run();
     }
 }
