7575a66e9e7c0cce94f18e4ba281c9fc04d933ab
Henrique Chaves <henriquechavesmm@gmail.com>
2022-11-22 18:00:27 +0100

    Create seed and factory for mastered_energy table

---
+++ pokemon/database/factories/MasteredEnergyFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\{Energy, Player};
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CombatStats>
+ */
+class MasteredEnergyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'energy_id' => Energy::all()->random()->id,
+            'player_id' => Player::all()->random()->id
+        ];
+    }
+}
--- pokemon/database/seeders/DatabaseSeeder.php
+++ pokemon/database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         CombatTypeSeeder::run();
         CombatStatsSeeder::run();
         CombatRoundSeeder::run();
+        MasteredEnergySeeder::run();
     }
 }
