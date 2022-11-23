6fada26a66967ba1a893360bef1c1abdfd0a4e26
Henrique Chaves <44180294+henchaves@users.noreply.github.com>
2022-11-23 09:02:22 +0100

    Update CombatRoundFactory.php

--- pokemon/database/factories/CombatRoundFactory.php
+++ pokemon/database/factories/CombatRoundFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\{Pokemon, Player, CombatStats};
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CombatStats>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CombatRound>
  */
 class CombatRoundFactory extends Factory
 {
