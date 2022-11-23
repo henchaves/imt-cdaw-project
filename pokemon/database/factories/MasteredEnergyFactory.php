35e5fe18b4e37a8c28cbf65808bd317ecc5ef02f
Henrique Chaves <44180294+henchaves@users.noreply.github.com>
2022-11-23 09:03:03 +0100

    Update MasteredEnergyFactory.php

--- pokemon/database/factories/MasteredEnergyFactory.php
+++ pokemon/database/factories/MasteredEnergyFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\{Energy, Player};
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CombatStats>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\MasteredEnergy>
  */
 class MasteredEnergyFactory extends Factory
 {
