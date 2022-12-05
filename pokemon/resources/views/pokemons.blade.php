ce50d0c6f3d402659fb79a03391c4b7b3e52a11f
Henrique Chaves <henriquechavesmm@gmail.com>
2022-12-05 16:55:54 +0100

    Enhance Pokemons Table + add navbar

--- pokemon/resources/views/pokemons.blade.php
+++ pokemon/resources/views/pokemons.blade.php
@@ -4,41 +4,47 @@
 
 @section('css')
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
+<link rel="stylesheet" href="{{asset('css/pokemons.css')}}">
 @endsection
 
 @section('content')
-<h1>Pokémons:</h1>
+<header>
+    <x-navbar />
+</header>
 
-<table class="table" id="pokemonsTable">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Energy</th>
-            <th>Level</th>
-            <th>HP</th>
-            <th>Normal Damage</th>
-            <th>Special Damage</th>
-            <th>Special Defense</th>
-            <th>Image</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($pokemons as $pokemon)
-        <tr>
-            <td>{{$pokemon->id}}</td>
-            <td>{{$pokemon->name}}</td>
-            <td>{{$pokemon->energy->name}}</td>
-            <td>{{$pokemon->level}}</td>
-            <td>{{$pokemon->max_health_points}}</td>
-            <td>{{$pokemon->normal_damage}}</td>
-            <td>{{$pokemon->special_damage}}</td>
-            <td>{{$pokemon->special_defense}}</td>
-            <td><img src="{{$pokemon->image_url}}" width="50px" height="50px" /></td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<main class="container">
+    <h3 class="text-center">Pokémons Table</h3>
+    <table class="table table-striped" id="pokemonsTable">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Energy</th>
+                <th>Level</th>
+                <th>HP</th>
+                <th>Normal Damage</th>
+                <th>Special Damage</th>
+                <th>Special Defense</th>
+                <th>Image</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($pokemons as $pokemon)
+            <tr>
+                <td>{{$pokemon->id}}</td>
+                <td>{{$pokemon->name}}</td>
+                <td>{{$pokemon->energy->name}}</td>
+                <td>{{$pokemon->level}}</td>
+                <td>{{$pokemon->max_health_points}}</td>
+                <td>{{$pokemon->normal_damage}}</td>
+                <td>{{$pokemon->special_damage}}</td>
+                <td>{{$pokemon->special_defense}}</td>
+                <td><img src="{{$pokemon->image_url}}" width="50px" height="50px" /></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</main>
 @endsection
 
 @section('js')
